398812607d7a6b2337b1547329b9ff4d85484bd8
Jack Caperon <jack.d.caperon@gmail.com>
2018-08-28 16:21:35 +1000

    Init

---
+++ app/src/main/java/com/example/jam/camerakitbug/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.jam.camerakitbug;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.camerakit.CameraKitView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CameraKitView cameraKitView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        cameraKitView = findViewById(R.id.camera);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraKitView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        cameraKitView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        cameraKitView.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+}
